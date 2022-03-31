3eeabda1de7f42cc58d6770ddd03ede371e477c0
tikkhun <s565338107@163.com>
2022-03-31 13:04:46 +0800

    test 分支 使用 clone 进行解决

--- redux-node/saga.test.js
+++ redux-node/saga.test.js
@@ -1,4 +1,5 @@
 import rootSaga, { downloadManager, startDownloadTask } from "./saga";
+import { cloneableGenerator } from "@redux-saga/testing-utils";
 import { select, put, call, take, fork, race, delay } from "redux-saga/effects";
 // FIXME 执行完毕 报错：  Cannot log after tests are done. Did you forget to wait for something async in your test?
 beforeAll(() => {
@@ -25,29 +26,37 @@ describe("test downloadManager", () => {
   const ACTIONS = {
     download: "DOWNLOAD",
   };
-  const gen = downloadManager();
+  // const gen = downloadManager();
+  const gen = cloneableGenerator(downloadManager)();
   test("wait to take action", (done) => {
     expect(gen.next().value).toEqual(take(ACTIONS.download));
     done();
   });
+
   test("the action is download,go to download", () => {
+    const clone = gen.clone();
     const action = {
       type: ACTIONS.download,
       payload: {
         url: "http://www.baidu.com",
       },
     };
-    expect(gen.next(action).value).toEqual(
+    expect(clone.next(action).value).toEqual(
       fork(startDownloadTask, action.payload.url, {})
     );
   });
-  test("the action is not download, just 重新 take 监听",()=>{
+  test("the action is not download, just 重新 take 监听action", () => {
+    const clone = gen.clone();
     const action = {
       type: "NOT_DOWNLOAD",
       payload: {
         url: "http://www.baidu.com",
       },
     };
-    expect(gen.next(action).value).toEqual(take(ACTIONS.download));
-  })
+    expect(clone.next(action).value).toEqual(take(ACTIONS.download));
+  });
+});
+
+describe("startDownloadTask", () => {
+  test("should yield race", () => {});
 });
