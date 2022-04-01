be9dbc0c951a8f476abe0fb012e0b2e8dff73c7c
tikkhun <s565338107@163.com>
2022-04-01 17:05:50 +0800

    fix timeout 函数的完善与测试

--- redux-node/__test__/timeout.test.js
+++ redux-node/__test__/timeout.test.js
@@ -1,47 +1,63 @@
 /**  */
-import { timeout } from "../saga";
+import { setFuncTimeout } from "../utils";
 import sagaTester from "../sagaTester";
 describe("timeout", () => {
+  jest.setTimeout(1000 * 60);
   test("正常执行 不会超时", (done) => {
     let fn = function* (value) {
       return 1;
     };
-
     sagaTester(
-      (error, result) => {
-        expect(error).toBeInstanceOf(Error);
-        expect(result).toBe(1);
-        done();
+      {
+        callback: (error, result) => {
+          expect(result).toBe(1);
+          expect(error).toBe(null);
+          done();
+        },
       },
-      timeout,
+      setFuncTimeout,
       // timeout
       fn,
-
       500
     );
   });
 
-  test("当执行时间超过预设的超时时间时，会触发超时错误", (done) => {
+  test("当执行时间超过预设的超时时间时，应触发超时错误", (done) => {
     let fn = function* (value) {
+      yield delay(1000);
       return 1;
     };
 
     sagaTester(
-      (error, result) => {
-        expect(error).toBeInstanceOf(Error);
-        expect(result).toBe(1);
-        done();
+      {
+        callback: (error, result) => {
+          expect(error).toBeInstanceOf(Error);
+          done();
+        },
       },
-      timeout,
+      setFuncTimeout,
       // timeout
       fn,
 
       500
     );
-    done();
   });
 
   test("执行函数出错误时，会抛出错误", (done) => {
-    done();
+    // 抛错的函数
+    let fn = function* () {
+      throw new Error("error");
+    };
+    sagaTester(
+      {
+        callback: (error, result) => {
+          expect(error).toBeInstanceOf(Error);
+          done();
+        },
+      },
+      setFuncTimeout,
+      fn,
+      500
+    );
   });
 });
--- redux-node/utils.js
+++ redux-node/utils.js
@@ -3,8 +3,12 @@
  * @param {*} fn
  * @param {*} interval
  */
-export function* setFuncTimeout(fn, interval, ...args) {
+export function* setFuncTimeout(fn, interval=0, ...args) {
   try {
+    // 不需要超时
+    if(interval === 0){
+      return call(fn,...args)
+    }
     let { isTimeout, result } = yield race({
       result: call(fn, ...args),
       isTimeout: delay(interval),
